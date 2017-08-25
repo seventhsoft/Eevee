4d1956a2eae2bcc7d7e703ee16c46a58b39fa3c9
nestor.arias <nesstorarias26@gmail.com>
2017-08-25 13:40:22 -0500

    concurso

--- src/main/java/com/lafortuna/delsaber/repository/ConcursoMapper.java
+++ src/main/java/com/lafortuna/delsaber/repository/ConcursoMapper.java
@@ -10,6 +10,8 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Result;
 import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+import org.springframework.dao.DataAccessException;
 
 /**
  *
@@ -17,18 +19,24 @@ import org.apache.ibatis.annotations.Select;
  */
 @Mapper
 public interface ConcursoMapper {
-    
+
     @Results(id = "concursoActual", value = {
         @Result(column = "id_concurso", property = "idConcurso", id = true),
         @Result(column = "fecha_inicio", property = "fechaInicio"),
         @Result(column = "fecha_fin", property = "fechaFin")
     })
-    @Select("select " +
-            "    id_concurso,       " +
-            "    c.fecha_inicio,    " +
-            "    c.fecha_fin        " +
-            "from concurso c        " +
-            "inner join estatus_concurso ec on ec.id_estado_concurso = c.id_estado_concurso and ec.id_estado_concurso = 1 " +
-            "where c.activo = false")
+    @Select("select "
+            + "    id_concurso,       "
+            + "    c.fecha_inicio,    "
+            + "    c.fecha_fin        "
+            + "from concurso c        "
+            + "inner join estatus_concurso ec on ec.id_estado_concurso = c.id_estado_concurso and ec.id_estado_concurso = 1 "
+            + "where c.activo = false")
     Concurso getFechaConcursoActual();
+
+    @Update("update concurso set id_estado_concurso = 4 where id_concurso = #{idConcurso} ")
+    void activarConcurso(Integer idConcurso) throws DataAccessException;;
+    
+    @Update("update concurso set id_estado_concurso = 4 where id_concurso = #{idConcurso} ")
+	void finalizarConcurso(Integer idConcurso) throws DataAccessException;
 }
